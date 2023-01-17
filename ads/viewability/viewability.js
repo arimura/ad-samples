baebf4d3fb26869838de066ed772e4125de2df79
arimura <kotaro_arimura@voyagegroup.com>
2023-01-17 14:28:56 +0900

    Add title

--- ads/viewability/viewability.js
+++ ads/viewability/viewability.js
@@ -1,13 +1,18 @@
-(function startViewability(){
+(function startViewability() {
     document.body.style.backgroundColor = "#ff0000";
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode("MRAID ads");
+    newDiv.appendChild(newContent);
+    document.body.appendChild(newDiv);
+
     const topConsole = window.top.console;
     const mraid = window.top.mraid;
     topConsole.log("start viewability");
-    if(mraid.getState() === "default"){
+    if (mraid.getState() === "default") {
         mraid.addEventListener(
             'exposureChange', (exposedPercentage, visibleRectangle, occlusionRectangles) => {
-                    topConsole.log("exposedPercentage: " + exposedPercentage);
-                }
+                topConsole.log("exposedPercentage: " + exposedPercentage);
+            }
         );
     }
 })();
